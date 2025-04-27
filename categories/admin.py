cb59cae333575286e34be4e6faa917da7f65e636
justjava <javapart6@gmail.com>
2025-04-27 12:44:12 +0500

    Add UserRegisterForm

--- categories/admin.py
+++ categories/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from categories.models import Category
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    list_filter = ['created_at']
+    list_per_page = 50
