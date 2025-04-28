b8602dbb7f610779da0dbc09cae4eb4bd3e71cc2
justjava <javapart6@gmail.com>
2025-04-28 14:51:43 +0500

    Add templates for new views

--- categories/urls.py
+++ categories/urls.py
@@ -3,5 +3,9 @@ from categories.views import *
 
 urlpatterns = [
     path('create/', CreateCategoryView.as_view(), name='category_create'),
+    path('update/<int:pk>', UpdateCategoryView.as_view(), name='category_update'),
+    path('delete/<int:pk>', DeleteCategoryView.as_view(), name='category_delete'),
+
     path('list/', ListCategoryView.as_view(), name='category_list'),
+    path('detail/<int:pk>', DetailCategoryView.as_view(), name='category_detail'),
 ]
