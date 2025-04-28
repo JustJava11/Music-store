29f2203176cb0465a622aad56d031fd3a78329a3
justjava <javapart6@gmail.com>
2025-04-28 23:18:37 +0500

    fix

--- profiles/models.py
+++ profiles/models.py
@@ -3,13 +3,13 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='username')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='username')
     description = models.TextField(null=True ,blank=True, verbose_name='description')
-    avatar = models.ImageField(upload_to='images/account_avatars/', default='account_avatars/default.jpg', blank=True, null=True)
+    avatar = models.ImageField(upload_to='media/images/account_avatars/', default='account_avatars/default.jpg', blank=True, null=True)
     registration_date = models.DateTimeField(default=timezone.now, verbose_name='registration_date')
 
     def __str__(self):
-        return self.username.username
+        return self.user.username
 
     class Meta:
         verbose_name_plural = 'profiles'
